e2ad3c0c3b5724241eac4c271d07d86491b78e8d
andres-norena <apuchigayqgmail.com>
2024-10-01 18:33:04 -0500

    hotfix

--- app/src/main/java/com/example/clase20243009/MainActivity.kt
+++ app/src/main/java/com/example/clase20243009/MainActivity.kt
@@ -6,19 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.clase20243009.DAO.UserDao
 import com.example.clase20243009.Database.UserDatabase
+import com.example.clase20243009.Repository.userRepository
 import com.example.clase20243009.Screen.UserApp
 
 class MainActivity : ComponentActivity() {
     private lateinit var userDao: UserDao
+    private lateinit var userRepository: userRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val db = UserDatabase.getDatabase(applicationContext)
         userDao = db.userDao()
+        userRepository = userRepository(userDao) // Inicializa userRepository
+
         enableEdgeToEdge()
         setContent {
-            UserApp(userDao)
+            UserApp(userRepository)
         }
     }
 }
