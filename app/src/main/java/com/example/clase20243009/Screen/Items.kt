648b5d6cca87d412379932a98da010f9ce791408
andres-norena <apuchigayqgmail.com>
2024-10-07 18:51:44 -0500

    correccion scroll horizontal

--- app/src/main/java/com/example/clase20243009/Screen/Items.kt
+++ app/src/main/java/com/example/clase20243009/Screen/Items.kt
@@ -6,12 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,6 +31,7 @@ fun UserApp(userRepository: userRepository) {
     var scope = rememberCoroutineScope()
     var context = LocalContext.current
     var users by remember { mutableStateOf(listOf<User>()) }
+    var showError by remember { mutableStateOf(false) } // Bandera para mostrar mensaje de error
 
     Box(
         modifier = Modifier
@@ -50,193 +46,221 @@ fun UserApp(userRepository: userRepository) {
             ),
         contentAlignment = Alignment.Center
     ) {
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .background(Color.White)
                 .padding(16.dp)
                 .fillMaxWidth(0.9f)
                 .fillMaxHeight(0.9f),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             // Título de la pantalla
-            Text(text = "Gestión de Usuarios", style = MaterialTheme.typography.headlineSmall)
+            item {
+                Text(text = "Gestión de Usuarios", style = MaterialTheme.typography.headlineSmall)
 
-            Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(16.dp))
+            }
 
             // Campo para nombre
-            TextField(
-                value = nombre,
-                onValueChange = { nombre = it },
-                label = { Text(text = "Nombre del Usuario") },
-                modifier = Modifier.fillMaxWidth()
-            )
+            item {
+                TextField(
+                    value = nombre,
+                    onValueChange = { nombre = it },
+                    label = { Text(text = "Nombre del Usuario") },
+                    modifier = Modifier.fillMaxWidth()
+                )
 
-            Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.height(8.dp))
+            }
 
             // Campo para apellido
-            TextField(
-                value = apellido,
-                onValueChange = { apellido = it },
-                label = { Text(text = "Apellido del Usuario") },
-                modifier = Modifier.fillMaxWidth()
-            )
+            item {
+                TextField(
+                    value = apellido,
+                    onValueChange = { apellido = it },
+                    label = { Text(text = "Apellido del Usuario") },
+                    modifier = Modifier.fillMaxWidth()
+                )
 
-            Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.height(8.dp))
+            }
 
             // Campo para edad
-            TextField(
-                value = edad,
-                onValueChange = { edad = it },
-                label = { Text(text = "Edad del Usuario") },
-                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                modifier = Modifier.fillMaxWidth()
-            )
+            item {
+                TextField(
+                    value = edad,
+                    onValueChange = { edad = it },
+                    label = { Text(text = "Edad del Usuario") },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                    modifier = Modifier.fillMaxWidth()
+                )
 
-            Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+            // Mostrar mensaje de error si algún campo está vacío
+            item {
+                if (showError) {
+                    Text(
+                        text = "Por favor, complete todos los campos",
+                        color = Color.Red,
+                        modifier = Modifier.padding(bottom = 8.dp)
+                    )
+                }
+            }
 
             // Botón para registrar usuario, se oculta si se está en modo edición
-            if (!isEditing) {
-                Button(
-                    onClick = {
-                        val user = User(
-                            nombre = nombre,
-                            apellido = apellido,
-                            edad = edad.toIntOrNull() ?: 0
-                        )
-                        scope.launch {
-                            withContext(Dispatchers.IO) {
-                                userRepository.insert(user)
+            item {
+                if (!isEditing) {
+                    Button(
+                        onClick = {
+                            if (nombre.isBlank() || apellido.isBlank() || edad.isBlank()) {
+                                showError = true
+                            } else {
+                                showError = false
+                                val user = User(
+                                    nombre = nombre,
+                                    apellido = apellido,
+                                    edad = edad.toIntOrNull() ?: 0
+                                )
+                                scope.launch {
+                                    withContext(Dispatchers.IO) {
+                                        userRepository.insert(user)
+                                    }
+                                    Toast.makeText(context, "Usuario registrado", Toast.LENGTH_SHORT).show()
+                                    // Limpiar los campos
+                                    nombre = ""
+                                    apellido = ""
+                                    edad = ""
+                                }
                             }
-                            Toast.makeText(context, "Usuario registrado", Toast.LENGTH_SHORT).show()
-                            // Limpiar los campos
-                            nombre = ""
-                            apellido = ""
-                            edad = ""
-                        }
-                    },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFB27300)),
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(text = "Registrar Usuario", color = Color.White)
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFB27300)),
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "Registrar Usuario", color = Color.White)
+                    }
+                } else {
+                    // Botón de actualizar visible solo en modo edición
+                    Button(
+                        onClick = {
+                            if (nombre.isBlank() || apellido.isBlank() || edad.isBlank()) {
+                                showError = true
+                            } else {
+                                showError = false
+                                val updatedUser = User(
+                                    id = idUpdate.toInt(),
+                                    nombre = nombre,
+                                    apellido = apellido,
+                                    edad = edad.toIntOrNull() ?: 0
+                                )
+                                scope.launch {
+                                    withContext(Dispatchers.IO) {
+                                        userRepository.update(updatedUser)
+                                    }
+                                    Toast.makeText(context, "Usuario actualizado", Toast.LENGTH_SHORT).show()
+                                    // Limpiar los campos y volver al modo no edición
+                                    nombre = ""
+                                    apellido = ""
+                                    edad = ""
+                                    idUpdate = ""
+                                    isEditing = false
+                                    // Actualizar la lista de usuarios
+                                    users = withContext(Dispatchers.IO) {
+                                        userRepository.getAllUser()
+                                    }
+                                }
+                            }
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4CAF50)),
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "Actualizar", color = Color.White)
+                    }
                 }
-            } else {
-                // Botón de actualizar visible solo en modo edición
+
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+            // Botón para listar usuarios
+            item {
                 Button(
                     onClick = {
-                        val updatedUser = User(
-                            id = idUpdate.toInt(),
-                            nombre = nombre,
-                            apellido = apellido,
-                            edad = edad.toIntOrNull() ?: 0
-                        )
                         scope.launch {
-                            withContext(Dispatchers.IO) {
-                                userRepository.update(updatedUser)
-                            }
-                            Toast.makeText(context, "Usuario actualizado", Toast.LENGTH_SHORT).show()
-                            // Limpiar los campos y volver al modo no edición
-                            nombre = ""
-                            apellido = ""
-                            edad = ""
-                            idUpdate = ""
-                            isEditing = false
-                            // Actualizar la lista de usuarios
                             users = withContext(Dispatchers.IO) {
                                 userRepository.getAllUser()
                             }
                         }
                     },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4CAF50)),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFB27300)),
                     modifier = Modifier.fillMaxWidth()
                 ) {
-                    Text(text = "Actualizar", color = Color.White)
+                    Text("Listar Usuarios", color = Color.White)
                 }
+
+                Spacer(modifier = Modifier.height(16.dp))
             }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Botón para listar usuarios
-            Button(
-                onClick = {
-                    scope.launch {
-                        users = withContext(Dispatchers.IO) {
-                            userRepository.getAllUser()
-                        }
-                    }
-                },
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFB27300)),
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Listar Usuarios", color = Color.White)
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
 
             // Mostrar lista de usuarios en tarjetas usando LazyColumn
-            LazyColumn(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                items(users) { user ->
-                    Card(
+            items(users) { user ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp),
+                    //elevation = 4.dp
+                ) {
+                    Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 4.dp),
-                        //elevation = 4.dp
+                            .padding(16.dp)
+                            .fillMaxWidth(),
+                        verticalArrangement = Arrangement.SpaceBetween
                     ) {
-                        Column(
-                            modifier = Modifier
-                                .padding(16.dp)
-                                .fillMaxWidth(),
-                            verticalArrangement = Arrangement.SpaceBetween
+                        Text(text = "ID: ${user.id}", style = MaterialTheme.typography.titleMedium)
+                        Text(text = "Nombre: ${user.nombre} ${user.apellido}", style = MaterialTheme.typography.bodyMedium)
+                        Text(text = "Edad: ${user.edad}", style = MaterialTheme.typography.bodyMedium)
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        // Botones de Editar y Eliminar
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
                         ) {
-                            Text(text = "ID: ${user.id}", style = MaterialTheme.typography.titleMedium)
-                            Text(text = "Nombre: ${user.nombre} ${user.apellido}", style = MaterialTheme.typography.bodyMedium)
-                            Text(text = "Edad: ${user.edad}", style = MaterialTheme.typography.bodyMedium)
-
-                            Spacer(modifier = Modifier.height(8.dp))
-
-                            // Botones de Editar y Eliminar
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.SpaceBetween
+                            Button(
+                                onClick = {
+                                    nombre = user.nombre
+                                    apellido = user.apellido
+                                    edad = user.edad.toString()
+                                    idUpdate = user.id.toString()
+                                    isEditing = true // Habilitar el modo edición
+                                },
+                                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4CAF50))
                             ) {
-                                Button(
-                                    onClick = {
-                                        nombre = user.nombre
-                                        apellido = user.apellido
-                                        edad = user.edad.toString()
-                                        idUpdate = user.id.toString()
-                                        isEditing = true // Habilitar el modo edición
-                                    },
-                                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4CAF50))
-                                ) {
-                                    Text(text = "Editar", color = Color.White)
-                                }
+                                Text(text = "Editar", color = Color.White)
+                            }
 
-                                Button(
-                                    onClick = {
-                                        val id = user.id
-                                        scope.launch {
-                                            val deletedCount: Int
-                                            withContext(Dispatchers.IO) {
-                                                deletedCount = userRepository.deleteById(id)
-                                            }
-                                            if (deletedCount > 0) {
-                                                Toast.makeText(context, "Usuario eliminado", Toast.LENGTH_SHORT).show()
-                                                // Actualizar la lista después de eliminar
-                                                users = withContext(Dispatchers.IO) {
-                                                    userRepository.getAllUser()
-                                                }
-                                            } else {
-                                                Toast.makeText(context, "Usuario no existe", Toast.LENGTH_SHORT).show()
-                                            }
+                            Button(
+                                onClick = {
+                                    val id = user.id
+                                    scope.launch {
+                                        val deletedCount: Int
+                                        withContext(Dispatchers.IO) {
+                                            deletedCount = userRepository.deleteById(id)
                                         }
-                                    },
-                                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFF44336))
-                                ) {
-                                    Text(text = "Eliminar", color = Color.White)
-                                }
+                                        if (deletedCount > 0) {
+                                            Toast.makeText(context, "Usuario eliminado", Toast.LENGTH_SHORT).show()
+                                            // Actualizar la lista después de eliminar
+                                            users = withContext(Dispatchers.IO) {
+                                                userRepository.getAllUser()
+                                            }
+                                        } else {
+                                            Toast.makeText(context, "Usuario no existe", Toast.LENGTH_SHORT).show()
+                                        }
+                                    }
+                                },
+                                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFF44336))
+                            ) {
+                                Text(text = "Eliminar", color = Color.White)
                             }
                         }
                     }
